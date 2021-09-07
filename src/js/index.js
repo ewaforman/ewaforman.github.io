c37baf2ee113d16bb39d1992b26ce9dc2ba576b0
Ewa Forman <ewaforman@gmail.com>
2021-09-07 10:36:57 +0200

    feat: index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,6 @@ fetch("https://api.github.com/users/ewaforman/repos?sort=created&direction=asc")
       console.log(repo);
       const { description, homepage, html_url, name } = repo;
 
-      let seeHere = "see here";
-
-      if (homepage == "") {
-        seeHere = "";
-      }
-
       const template = `<article class="project">
         <div class="project__window">
           <span class="project__circle"></span>
@@ -38,19 +32,49 @@ fetch("https://api.github.com/users/ewaforman/repos?sort=created&direction=asc")
               ></span
             >
           </p>
-          <p class="project__grid project__grid--js">
+          <p class="project__grid">
             <span class="project__label">demo: </span>
             <span>
               
-              <a class="project__link" href="${homepage}" title="${name}">${seeHere}</a
+              <a class="project__link" href="${homepage}" title="${name}">seeHere</a
               ></span
             >
           </p>
         </div>
       </article>`;
 
-      if (description) {
+      const templateWithoutHomepage = `<article class="project">
+        <div class="project__window">
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+          <span class="project__circle"></span>
+        </div>
+        <div class="project__content">
+          <img src="img/github-cat.svg" alt="" />
+          <h3 class="project__grid project__title">
+            <span class="project__label">project:</span>
+            <span>${name}</span>
+          </h3>
+          <p class="project__grid project__grid">
+            <span class="project__label">description: </span>
+            <span> ${description} </span>
+          </p>
+          <p class="project__grid">
+            <span class="project__label">Github: </span>
+            <span>
+              <a class="project__link" href="${html_url}" title="${name}">
+                source_code</a
+              ></span
+            >
+          </p> 
+        </div>
+      </article>`;
+
+      if (description && homepage) {
         conteiner.innerHTML += template;
+      }
+      if (description && !homepage) {
+        conteiner.innerHTML += templateWithoutHomepage;
       }
     }
   })
